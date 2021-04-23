29e15086ca1288211bc0005cca6694faf67ea224
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2021-04-23 21:20:32 +0100

    add some stuff

--- src/insultCommands.py
+++ src/insultCommands.py
@@ -8,28 +8,28 @@ class insultCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.insults = []
-        data = urllib.request.urlopen("https://pr0x1mas.github.io/InsultBot/src/insults.csv")
+        data = urllib.request.urlopen("https://pr0x1mas.github.io/InsultBot/src/insults.csv") # get insults from online list
         for line in data:
             self.insults.append(line.decode("utf-8"))
-            print(line.decode("utf-8"))
+            print(line.decode("utf-8")) # print all insults
         
 
 
     @commands.command(brief="Insult someone", description="Insult someone in the format `?insult @user-to-insult`")
     async def insult(self, ctx, person):
         if person == "@everyone":
-            await ctx.send(random.choice(self.insults).replace("?1", ctx.author.mention).replace("?2", "everyone"))
+            await ctx.send(random.choice(self.insults).replace("?1", ctx.author.mention).replace("?2", "everyone").replace("\"", "")) # if someone tries to insult everyone, insult them instead
         elif person.replace("!", "") == self.bot.user.mention:
-            await ctx.send(random.choice(self.insults).replace("?1", ctx.author.mention).replace("?2", self.bot.user.mention))
+            await ctx.send(random.choice(self.insults).replace("?1", ctx.author.mention).replace("?2", self.bot.user.mention).replace("\"", "")) # the bot cannot insult itself, that is a sign of weakness
         else:
-            await ctx.send(random.choice(self.insults).replace("?1", person).replace("?2", ctx.author.mention))
+            await ctx.send(random.choice(self.insults).replace("?1", person).replace("?2", ctx.author.mention).replace("\"", ""))
 
     @commands.command()
-    async def reload(self, ctx):
+    async def reload(self, ctx): # reload all insults
         await ctx.send("reloading insults...")
         async with ctx.typing():
             self.insults = []
-            data = urllib.request.urlopen("https://pr0x1mas.github.io/InsultBot/src/insults.csv")
+            data = urllib.request.urlopen("https://pr0x1mas.github.io/InsultBot/src/insults.csv") # get insults from online list
             for line in data:
                 self.insults.append(line.decode("utf-8"))
                 print("loaded insult " + line.decode("utf-8"))
