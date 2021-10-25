dc08683a8b856e7497ed4083c46ce5dcdbc38971
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2021-10-25 20:22:05 +0100

    fuck sake

--- src/main.py
+++ src/main.py
@@ -34,7 +34,6 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(activity=discord.Game(name="spansh.co.uk"))
     
 @slash.slash(
 name="Insult",
